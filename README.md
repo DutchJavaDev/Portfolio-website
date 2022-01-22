a0c89c914c1e9e0612ce60359e26bb83f9be545c
Boris <Borismulder@outlook.com>
2022-01-22 16:29:58 +0100

    Project create

---
+++ README.md
@@ -0,0 +1,10 @@
+# Project
+This is my personal portfolio project that i will be developing in svelte, svelte is easy to use and is pretty fast to.
+The website will contain deatails about me and my programming project, experiences and more about me. This readme file will be updated when needed.
+>
+[Project Planning](https://trello.com/b/eu0Afki1/portfolio)
+>
+[Svlete compiler/framework](https://svelte.dev/)
+
+
+
---
+++ src/App.svelte
@@ -0,0 +1,49 @@
+<script>
+    //Svelte import
+    import { 
+        Fullpage,
+        FullpageSection,
+        FullpageSlide
+    } from 'svelte-fullpage';
+    //Optional, include all titles of your sections, this is also used as number that indicate count of sections
+    const sections = [
+        'Landing',
+        'Projects',
+        'Experiences',
+        'About me'
+    ];
+
+	// Project slides
+    const slides = [
+        'one',
+        'two',
+        'three'
+    ];
+</script>
+
+<Fullpage {sections} arrows>
+
+    <FullpageSection center>
+        Landing
+    </FullpageSection>
+
+    <FullpageSection {slides} arrows>
+        <FullpageSlide>
+            Projects#
+        </FullpageSlide>
+        <FullpageSlide>
+            Projects#
+        </FullpageSlide>
+        <FullpageSlide>
+            Projects#
+        </FullpageSlide>
+    </FullpageSection>
+
+    <FullpageSection>
+        Experiences
+    </FullpageSection>
+	
+    <FullpageSection>
+        About me
+    </FullpageSection>
+</Fullpage>
