6fc94bb87730a7abac9c89870df3e63b5842d4a3
Boris <Borismulder@outlook.com>
2022-01-26 21:37:42 +0100

    Project display

---
+++ src/Components/ProjectCard.svelte
@@ -0,0 +1,63 @@
+<script>
+    export let projectTitle;
+    export let projectDescription;
+    export let projectLink;
+</script>
+
+<div class="card-container">
+    <div class="card-title">{projectTitle}</div>
+    <hr>
+    <div class="card-description">{projectDescription}</div>
+    <div class="card-link">
+        <button>{projectLink}</button>
+    </div>
+</div>
+
+<style>
+    .card-container {
+        display: grid;
+        grid-template-columns: auto;
+        border: 3px solid white;
+        border-radius: 5px;
+    }
+
+    hr{
+        background-color: white !important;
+        width: 100%;
+        height: 3px;
+    }
+
+    .card-title {
+        font-size: 24px;
+        text-align: center;
+    }
+
+    .card-link,
+    .card-description {
+        font-size: 18px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .card-description {
+        margin-top: 0px;
+        height: 15rem;
+    }
+
+    .card-link{
+        margin-bottom: 10px;
+    }
+
+    .card-link > button {
+        width: 100%;
+        height: 2.5rem;
+        color: white;
+        font-size: 1.1rem;
+        font-family: inherit;
+        background-color: #828EB6;
+        border-radius: 5px;
+        border: none;
+        cursor: pointer;
+        /* do hover and click effects */
+    }
+</style>
---
+++ src/Components/Projects.svelte
@@ -0,0 +1,54 @@
+<script>
+    // load from gihub or api..
+    import ProjectCard from './ProjectCard.svelte';
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+
+    });
+
+    function mouseleave(e) {
+        console.log(e);
+    }
+
+    let projects = [
+        {
+            title:"Project title",
+            description:"Project description",
+            link:"View on github",
+        },
+        {
+            title:"Project title",
+            description:"Project description",
+            link:"View on github",
+        },
+        {
+            title:"Project title",
+            description:"Project description",
+            link:"View on github",
+        }
+    ];
+</script>
+    <div class="project-title">Projects</div>
+    <div class="project-container" on:mouseleave={mouseleave}>
+        {#each projects as project}
+        <ProjectCard projectTitle={project.title} projectDescription={project.description} projectLink={project.link} />
+        {/each}
+    </div>
+<style>
+.project-title {
+    text-align: center;
+    font-size: 2.5rem;
+}
+
+.project-container {
+    overflow-y: auto;
+    display: grid;
+    grid-column-gap: 10rem;
+    grid-row-gap: 2rem;
+    margin-top: 12.5%;
+    margin-left: 5rem;
+    margin-right: 5rem;
+    grid-template-columns: auto auto auto;
+}
+</style>
