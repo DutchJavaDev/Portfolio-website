99c1f9c347c0c513feb7d2cf4fb3e52b8b47e4d1
Boris <Borismulder@outlook.com>
2022-01-26 21:37:59 +0100

    Todo but just created them

---
+++ src/Components/About.svelte
@@ -0,0 +1,8 @@
+<div class="about-title">About me</div>
+<footer>Footer place image down here</footer>
+<style>
+    .about-title {
+    text-align: center;
+    font-size: 2.5rem;
+}
+</style>
---
+++ src/Components/Experience.svelte
@@ -0,0 +1,7 @@
+<div class="experience-title">Experiences</div>
+<style>
+    .experience-title {
+    text-align: center;
+    font-size: 2.5rem;
+}
+</style>
