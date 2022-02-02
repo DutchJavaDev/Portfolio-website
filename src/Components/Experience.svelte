83ae356cacb34c8cb0f68b2662b4ba943d22d65c
Boris <Borismulder@outlook.com>
2022-02-02 16:20:46 +0100

    fixed bug with layout not being the right size, forgot to add display:grid :D

--- src/Components/Experience.svelte
+++ src/Components/Experience.svelte
@@ -1,7 +1,27 @@
+<script>
+  import ExperienceCard from './ExperienceCard.svelte'
+  export let experiences;
+
+</script>
 <div class="experience-title">Experiences</div>
+<div class="experience-container">
+  {#each experiences as exp}
+  <ExperienceCard experienceYear={exp.year} experienceTitle={exp.title} experienceDescription={exp.description}/>
+{/each}
+</div>
+
 <style>
-    .experience-title {
-    text-align: center;
+  .experience-title {
     font-size: 2.5rem;
-}
+    text-align: center;
+  }
+  .experience-container {
+    overflow-y: auto;
+    display: grid;
+    grid-column-gap: 5rem;
+    margin-top: 12.5%;
+    margin-left: 5rem;
+    margin-right: 5rem;
+    grid-template-columns: 1fr 1fr 1fr;
+  }
 </style>
---
+++ src/Components/ExperienceCard.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let experienceYear;
+    export let experienceTitle;
+    export let experienceDescription;
+</script>
+
+<div class="card-container">
+    <div class="card-year">{experienceYear}</div>
+    <br>
+    <br>
+    <div class="card-title">{experienceTitle}</div>
+    <br>
+    <div class="card-description">{experienceDescription}</div>
+</div>
+
+<style>
+    .card-container {
+        display: grid;
+        grid-template-columns: auto;
+        border: 2px solid white;
+        border-radius: 5px;
+    }
+
+    .card-title, .card-year {
+        font-size: 22px;
+        text-align: left;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .card-year {
+        margin-top: 2px;
+        font-size: 24px;
+    }
+
+    .card-description {
+        font-size: 18px;
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .card-description {
+        margin-top: 0px;
+        height: 15rem;
+    }
+
+    .card-link{
+        margin-bottom: 10px;
+    }
+</style>
